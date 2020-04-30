b4c2b03c589450a90f65119ac74b981074ede13d
Zyf <Zyfarok@users.noreply.github.com>
2020-04-30 20:39:32 +0200

    Capitalize keywords

--- cleanup.sql
+++ cleanup.sql
@@ -2,7 +2,7 @@ DELETE FROM osu_scores_high WHERE hidden!=0;
 
 -- Saving the id of the scores we delete :
 CREATE TABLE duplicate_scores(score_id int(9) PRIMARY KEY NOT NULL);
-INSERT INTO duplicate_scores SELECT A.score_id FROM osu_scores_high as A WHERE EXISTS (
+INSERT INTO duplicate_scores SELECT A.score_id FROM osu_scores_high AS A WHERE EXISTS (
     SELECT * FROM osu_scores_high AS B WHERE (
         A.score_id!=B.score_id AND A.beatmap_id=B.beatmap_id AND A.enabled_mods=B.enabled_mods AND A.user_id=B.user_id
     ) AND (
@@ -14,8 +14,8 @@ DELETE FROM osu_scores_high WHERE score_id IN (SELECT * FROM duplicate_scores);
 
 -- Alternative :
 -- DELETE FROM osu_scores_high WHERE score_id IN (
---     SELECT A.score_id FROM (select * from osu_scores_high) as A WHERE EXISTS (
---         SELECT * FROM (select * from osu_scores_high) as B WHERE (
+--     SELECT A.score_id FROM (SELECT * FROM osu_scores_high) AS A WHERE EXISTS (
+--         SELECT * FROM (SELECT * FROM osu_scores_high) AS B WHERE (
 --             A.score_id!=B.score_id AND A.beatmap_id=B.beatmap_id AND A.enabled_mods=B.enabled_mods AND A.user_id=B.user_id
 --         ) AND (
 --             A.score<B.score OR (A.score=B.score AND A.score_id<B.score_id))
